6eb21de28fb6149776be479c9be1173b56c824ce
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-11-21 17:48:16 -0500

    Setup root component with global state and routing.

---
+++ tour-comparison-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Gallery from "./Gallery";
+
+function App() {
+  return (
+    <div>
+      <h1>Tour Comparison</h1>
+      <Gallery />
+    </div>
+  );
+};
+//acting as a container for the gallery/its functions.
+//initializes
+
+export default App;
